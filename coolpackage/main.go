a38ea8495b38a4300b16bcbd46560892279f32b2
AislingHPE <aisling.heanue@hpe.com>
2023-11-06 15:15:19 +0000

    Added a line to go.mod

--- coolpackage/main.go
+++ coolpackage/main.go
@@ -3,7 +3,7 @@ package coolpackage
 import ver "github.com/AislingHPE/simple-go-package/pkg/interfaces"
 
 var v = ver.VersionHolder{
-	Version: "v0.0.4",
+	Version: "v0.0.5",
 }
 
 func GetVersion() string {
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/AislingHPE/simple-go-package
 
 go 1.20
+
+require github.com/AislingHPE/simple-go-package/pkg v0.0.0
